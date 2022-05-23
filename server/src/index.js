f1c451c8e4a84097cb5a1e1d82ea62a561297101
vasa-hadzheha <73876508+vasa-hadzheha@users.noreply.github.com>
2022-05-23 18:30:23 +0300

    create routes for channels and update options

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,26 @@
-const express = require('express')
-const app = express()
-const options = require("./options")
+const express = require('express');
+const options = require("./options");
+const channelRouter = require("../telegramChannels")
 
+const cors = require("cors");
 
-app.listen(options.port,()=>{
-    console.log(`Backend server was started on http://localhost:${options.port}`)
+const backendServer = express()
+
+backendServer.use(
+    cors({
+        origin:`http://${options.clientIP}:${options.clientPort}`,
+    })
+);
+
+backendServer.use(express.urlencoded({extended: false}));
+backendServer.use(express.json());
+
+backendServer.use("/telegram-channels", channelRouter);
+
+backendServer.all("*", (req,res)=>{
+    res.status(404).send("URL not found");
+});
+
+backendServer.listen(options.serverPort,()=>{
+    console.log(`Backend server was started on http://localhost:${options.serverPort}`);
 })
--- server/src/options.js
+++ server/src/options.js
@@ -1,7 +1,9 @@
 module.exports={
-    port: 3000,
+    serverPort: 3000,
     retryInterval: 1000,
-    dbPath: "./data/Telegram-channels.db",
+    dbPathTelegramChannels: "./data/TelegramChannels.db",
     token: "5245684585:AAHrIiqn-F83euFvX2YxxZeRw5u5XpeZn2g",
-    telegramPort: "3001"
+    telegramPort: "3001",
+    clientPort: 5000,
+    clientIP: "127.0.0.42",
 }
